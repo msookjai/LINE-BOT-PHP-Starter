01d2458f7041fe468dfdef31c3871c04d40c940f
msookjai <msookjai@gmail.com>
2017-03-14 15:59:17 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = 'M4ikzreXaC0LRBqMqP1Pu64ncgfT2dSUH2Lht2QCiLzAqrYv5sMmXrC6FOUB6dm6DL0ML8bsLtR/Y1EvqCC5ycB8iSQvBo4z34xYcN/vqxrE1/p021/5Hs10OblHrPq2nNDaSrJcdM8N16eYnwbPjwdB04t89/1O/w1cDnyilFU=';
-$content = $_GET['zbxreport'];
+$content = $_POST['zbxreport'];
 $to = 'C4525be31a312a6a0a25d7c0c340315d8';
 $url = 'https://api.line.me/v2/bot/message/push';
 $messages = [
